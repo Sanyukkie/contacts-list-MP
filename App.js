3ecb3fe827033bade56a08b79dbbe91d1f3b709f
Aleksander Truss <aleksander.truss@nordhealth.com>
2022-10-10 17:35:37 +0300

    Exercise complete

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import * as Contacts from 'expo-contacts';
+
 
 export default function App() {
+
+  const [contact, setContact] = useState({});
+  
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync(      
+        { fields: [Contacts.Fields.PhoneNumbers] }    
+        );
+        if (data.length > 0) {
+            setContact(data);    
+        }  
+      }
+    }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <FlatList style={styles.list}
+        data={contact}
+        renderItem={({ item }) =>
+          <View style={styles.item}>
+            <Text>{item.name}</Text>
+            <Text>{item.phoneNumbers[0].number}</Text>
+          </View>
+        }
+      />
+
+      <Button title="Get Contact"onPress={getContacts} />
     </View>
   );
 }
@@ -16,5 +44,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop:100
+  },
+  item: {
+    alignItems: 'center',
   },
 });
